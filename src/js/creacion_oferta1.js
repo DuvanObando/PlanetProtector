958a32b0fad3cd35cd04b079620c9edd1f85b418
DuvanObando <duvan.luffy@gmail.com>
2024-05-07 15:39:51 -0500

    soluciones

--- src/js/creacion_oferta1.js
+++ src/js/creacion_oferta1.js
@@ -2,6 +2,8 @@
 
 const iconmenu = document.querySelector('.bx_menu')
 const menu = document.querySelector('.menu')
+const oferta = document.querySelector('.oferta')
+
 
 // La herramienta toggle se encarga de verificar si una propiedad o en este caso una clase esta o no, dado caso si esta la quita y si no la crea
 iconmenu.addEventListener('click', function(){
